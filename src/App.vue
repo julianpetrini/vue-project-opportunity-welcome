cc7521507b0b541f68eecb842b4741531129f9e1
julianpetrini <bwmisojos@gmail.com>
2022-05-16 15:51:01 +0200

    layout ok

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
 <template>
-<body>
-  <div id="app">
+  <body>
+    <div id="app">
       <div class="top">
         <h1 class="title">Welcome to Opportunity</h1>
-        <h2 class="date">{{currentDateTime()}}</h2>
+        <h2 class="date">{{ currentDateTime() }}</h2>
       </div>
       <ul class="main">
         <li class="bloque">
-          <span class="date-box">14.00 Uhr</span>
-          <span class="tittle-box">Basisbeschäftigung Besuch</span>
-          <span class="description-box">
-            Interessierte für zweiten Kurs werden uns besuchen
-          </span>
+          <span class="date-box">{{ time }}</span>
+          <span class="tittle-box">{{ title }}</span>
+          <span class="description-box">{{ description }}</span>
         </li>
         <li class="bloque">
-          <span class="date-box">14.00 Uhr</span>
-          <span class="tittle-box">Basisbeschäftigung Besuch</span>
-          <span class="description-box">Interessierte für zweiten Kurs werden uns besuchen</span>
+          <span class="date-box">{{ time }}</span>
+          <span class="tittle-box">{{ title }}</span>
+          <span class="description-box">{{ description }}</span>
         </li>
         <li class="bloque">
-          <span class="date-box">14.00 Uhr</span>
-          <span class="tittle-box">Basisbeschäftigung Besuch</span>
-          <span class="description-box">Interessierte für zweiten Kurs werden uns besuchen</span>
+          <span class="date-box">{{ time }}</span>
+          <span class="tittle-box">{{ title }}</span>
+          <span class="description-box">{{ description }}</span>
         </li>
       </ul>
-    <footer>
-      <img class="img-footer" alt="stadt zürich logo" src="./assets/STZH_SEB_Logo.png"/>
-      <img class="img-footer" alt="Opportunity logo"  src="./assets/Opportunity.png"/>
-      <img class="img-footer" alt="Sag logo" src="./assets/SAG_Logo_De.png" />
-    </footer>
-  </div>
+      <footer>
+        <img
+          class="img-footer"
+          alt="stadt zürich logo"
+          src="./assets/STZH_SEB_Logo.png"
+        />
+        <img
+          class="img-footer"
+          alt="Opportunity logo"
+          src="./assets/Opportunity.png"
+        />
+        <img class="img-footer" alt="Sag logo" src="./assets/SAG_Logo_De.png" />
+      </footer>
+    </div>
   </body>
 </template>
 
 <script>
-
 //Here also could go import, to acces to different components.
 
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      time: "14:00 Uhr",
+      title: "Basisbeschäftigung Besuch",
+      description: "Interessierte für zweiten Kurs werden uns besuchen",
+    };
+  },
 
   methods: {
     currentDateTime() {
@@ -52,9 +63,9 @@ export default {
         return dia + "." + "0" + mes + "." + year;
       }
       return dateTime;
-        },
-      },
-    };
+    },
+  },
+};
 </script>
 
 <style>
@@ -68,7 +79,6 @@ export default {
   color: #2c3e50;
   margin: 0;
   padding: 0;
-   
 }
 /* DIV FOR HEADER */
 .top {
@@ -93,6 +103,7 @@ export default {
   margin: 0;
   padding: 0;
   height: 80vh;
+  width: 100%;
 }
 .bloque {
   background-color: #0f05a0;
@@ -118,15 +129,15 @@ export default {
 }
 .description-box {
   color: #ffbaab;
-  
+  font-size: 28px;
 }
 
 /* FOOTER */
 footer {
   position: fixed;
-  bottom: 0;  
+  bottom: 0;
   width: 100vw;
-  background-color:white;
+  background-color: rgb(255, 255, 255);
   display: flex;
   justify-content: space-around;
   /* display: grid;  height: 95vh;
@@ -140,4 +151,41 @@ footer {
   align-items: center;
 }
 
+/* RESPONSIVE */
+
+@media screen and (max-width: 600px) {
+  body {
+    background-color: #e6eff4;
+    color: white;
+  }
+  .title {
+    font-size: 30px;
+    font-weight: 900;
+  }
+  .tittle-box {
+    color: #ffbaab;
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 36px;
+  }
+  .date {
+    color: #9aa7b1;
+    font-size: 20px;
+    font-weight: 500;
+  }
+  .img-footer {
+    padding: 1rem;
+    width: auto;
+    height: 2vh;
+    object-fit: contain;
+    align-items: center;
+  }
+  footer {
+    width: 100%;
+    position: fixed;
+    bottom: 0px;
+    left: 0px;
+    background-color: rgb(255, 255, 255);
+  }
+}
 </style>
